b50458bff372f84a276bb891692860c1f55e13a6
AntonBrito <tonybrito85@gmail.com>
2018-07-27 12:35:47 -0400

    Create and set userSchema

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.export = User = mongoose.model("users", userSchema);
